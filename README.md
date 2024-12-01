51775bca274bf44b1bba06f6d964ae94ad4692a8
aniceto-jolela <anicetojosejolelaj@gmail.com>
2024-12-01 12:10:53 +0100

    add group

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # django-api-rest-framework
 API Creation Using Django Rest Framework.
+
+
+[CONFIG-DJANGO-API-REST-FRAMEWORK](https://github.com/aniceto-jolela/tips/blob/main/django/DJANGO-API-REST-FRAMEWORK.md)
--- apirest/apirest/urls.py
+++ apirest/apirest/urls.py
@@ -17,26 +17,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
-
-
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'is_staff']
-
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
+from rest_framework import routers
+from website import views as views_user
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'users', views_user.UserViewSet)
+router.register(r'groups', views_user.GroupViewSet)
+
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
